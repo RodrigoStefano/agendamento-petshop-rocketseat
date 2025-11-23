e295943a97f489b234b718210bee157686561600
RodrigoStefano <rodrigostefano65@gmail.com>
2025-11-23 11:26:32 -0300

    Conclusão estrutura base do modal de agendamento

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnScheduleDesktop = document.getElementById("btnNewScheduleDesktop")
 const btnScheduleMobile = document.getElementById("btnNewScheduleMobile")
 const btnCloseModal = document.getElementById("btn-close-modal")
+const btnSchedule = document.getElementById("btn-schedule")
 
 const modalContainer = document.getElementById("modal-container")
 
@@ -8,6 +9,11 @@ const modalContainer = document.getElementById("modal-container")
 btnScheduleDesktop.addEventListener("click", ShowModal)
 btnScheduleMobile.addEventListener("click", ShowModal)
 btnCloseModal.addEventListener("click", CloseModal)
+btnSchedule.addEventListener("click", (event) => {
+  event.preventDefault()
+
+  alert("Agendamento criado com sucesso")
+})
 
 /* FUNÇÕES MODAL */
 function ShowModal() {
